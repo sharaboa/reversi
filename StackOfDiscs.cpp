7b1ceec673159147a67f657993a55a43026d58ae
sharaboa <thhbayhhia@walla.co.il>
2017-11-23 12:38:48 +0200

    operation overloading

--- StackOfDiscs.cpp
+++ StackOfDiscs.cpp
@@ -18,7 +18,7 @@ void StackOfDiscs::addToStack(int i, int j) {
 
 void StackOfDiscs::removeFromStack(Disc &disc) {
     for(int i = 0; i <= stack.size(); i++) {
-        if (stack[i].isSimilar(disc)) {
+        if (stack[i] == disc) {
             stack.erase(stack.begin() + i);
             break;
         }
@@ -37,7 +37,7 @@ Disc StackOfDiscs::getDisc(int x) const {
 void StackOfDiscs::isRepeat() {
     for(int i = 0; i < stack.size(); i++) {
         for (int j = i + 1; j < stack.size();) {
-            if(stack[i].isSimilar(stack[j])) {
+            if(stack[i] == stack[j]) {
                 stack.erase(stack.begin() + j);
             }
             else {
